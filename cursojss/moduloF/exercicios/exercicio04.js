5656704e3ec0829a0967634ab0277d18ed2c1042
Conrado Vinha <conradovinha@gmail.com>
2023-07-07 23:09:48 -0300

    js

---
+++ cursojss/moduloF/exercicios/exercicio04.js
@@ -0,0 +1,14 @@
+function contar(){
+    let inicio = document.getElementById('inicio');
+    let fim = document.getElementById('fim');
+    let passo = window.document.querySelector('passo');
+
+    if (inicio.value.lenght == 0){
+        window.alert('ERRO');
+    }
+    else{
+        if(inicio.value ){
+
+        }
+    }
+}
